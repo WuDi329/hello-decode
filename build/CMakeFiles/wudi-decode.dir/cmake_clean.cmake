3f7414cce3fd202eaf9ecd51ef38a781ca5ee543
wudi-ubuntu <1582074055@qq.com>
2022-07-12 13:14:29 +0800

    first commit

---
+++ build/CMakeFiles/wudi-decode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wudi-decode.dir/src/hello-decode.cpp.o"
+  "wudi-decode.pdb"
+  "wudi-decode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wudi-decode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
